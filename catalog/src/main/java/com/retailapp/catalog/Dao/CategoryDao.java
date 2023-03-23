be421cb477923ad621cd9ad4551dbf1747937367
shivendu-2311 <81577820+shivendu-2311@users.noreply.github.com>
2023-03-23 23:12:44 +0530

    Delete catalog directory

--- catalog/src/main/java/com/retailapp/catalog/Dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.retailapp.catalog.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.retailapp.catalog.POJO.Category;
-
-public interface CategoryDao extends JpaRepository<Category,Integer> {
-
-}
